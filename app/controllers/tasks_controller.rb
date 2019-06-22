2678a17e693ce9064a24fd2c7300f9ce834a71ce
ruru.tomo <nickel-back.1286@outlook.jp>
2019-06-22 10:20:11 +0000

    tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,17 +1,19 @@
 class TasksController < ApplicationController
   def index
-      @tasks = Task.all
+    @tasks = Task.all
   end
 
   def show
-      @task = Task.find(params[:id])
+    @task = Task.find(params[:id])
   end
 
   def new
-      @task = Task.new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+    
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
       redirect_to @task
@@ -22,11 +24,11 @@ class TasksController < ApplicationController
   end
 
   def edit
-      @task = Task.find(params[:id])
+    @task = Task.find(params[:id])
   end
 
   def update
-     @task = Task.find(params[:id])
+    @task = Task.find(params[:id])
 
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
