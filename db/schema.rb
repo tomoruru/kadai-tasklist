5cb43f24a9a27d1e574fbf50f050c336d8e204d7
ruru.tomo <nickel-back.1286@outlook.jp>
2019-06-23 12:38:46 +0000

    tasklist

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_19_130343) do
+ActiveRecord::Schema.define(version: 2019_06_23_083343) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.string "status"
   end
 
 end
