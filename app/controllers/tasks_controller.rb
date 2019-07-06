9863b8e89986ca39102762a35c2829caf849ee2c
ruru.tomo <nickel-back.1286@outlook.jp>
2019-07-06 14:01:31 +0000

    for heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks
   end
 
   def show
@@ -40,17 +40,24 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
   end
   
   private
+  
   def set_task
     @task = Task.find(params[:id])
   end
 
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to tasks_url
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,4 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
-  
-  def index
-    @users = User.order(id: :desc).page(params[:page]).per(25)
-  end
-
-  def show
-    @user = User.find(params[:id])
-    @users = @user.users.order(id: :desc).page(params[:page])
-    counts(@user)
-  end
 
   def new
     @user = User.new
@@ -28,23 +16,9 @@ class UsersController < ApplicationController
     end
   end
   
-  def destroy
-    @user.destroy
-    flash[:success] = 'メッセージを削除しました。'
-    redirect_back(fallback_location: root_path)
-  end
-  
   private
   
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
-  
-  def correct_user
-    @user = current_user.users.find_by(id: params[:id])
-    unless @user
-      redirect_to root_url
-    end
-  end
-  
 end
