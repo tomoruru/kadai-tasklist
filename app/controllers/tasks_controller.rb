1b4fdc0c8b517f78fceb2c56dbc12efd5c426a4d
ruru.tomo <nickel-back.1286@outlook.jp>
2019-07-08 12:45:16 +0000

    tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,8 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
+  
   
   def index
     @tasks = current_user.tasks
@@ -57,7 +59,7 @@ class TasksController < ApplicationController
   def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
     unless @task
-      redirect_to tasks_url
+      redirect_to root_url
     end
   end
 end
